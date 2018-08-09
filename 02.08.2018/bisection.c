ee0f131782520b7748278d0bbeeb7fcedab234b1
mihirsam <xenon.of.x@gmail.com>
2018-08-09 13:35:10 +0530

    partial complete bisection method problem : infinite loop

---
+++ 02.08.2018/bisection.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<math.h>
+
+int count = 0, decimalPlace = 3;
+float num1 = 0.0, num2 = 1.0, mean;
+
+float fun(float num)
+{
+  return (pow(num, 3) - (2 * num) - 5);
+}
+
+
+int main()
+{
+  while(1)
+  {
+    if((fun(num1) < 0 && fun(num2) > 0) || (fun(num1) > 0 && fun(num2) < 0))
+    {
+      printf("Root lies between (%0.3f, %0.3f)\n", num1, num2);
+      break;
+    }
+
+    else
+    {
+      num1++;
+      num2++;
+    }
+  }
+
+  printf("Bisection Method :\nSl.No.\t\tRoot1\t\tRoot2\n");
+  while(1)
+  {
+    mean = (num1 + num2)/2.0;
+
+    if(fun(mean) > 0)
+    {
+      num2 = mean;
+    }
+    else
+    {
+      num1 = mean;
+    }
+
+    printf("%d\t\t%f\t\t%f", count, num1, num2);
+    count++;
+
+    if((100 * num1) == (100 * num2))
+    {
+      break;
+    }
+  }
+
+  return 0;
+}
